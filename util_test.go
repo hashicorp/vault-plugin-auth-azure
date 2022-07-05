438dd29a60b816485eb18dbd11d212997ad88c51
Milena Zlaticanin <milena@hashicorp.com>
2022-07-05 10:43:15 -0500

    remove partner id

--- util_test.go
+++ util_test.go
@@ -3,11 +3,8 @@ package azureauth
 import (
 	"encoding/json"
 	"fmt"
-	"strings"
 	"testing"
 	"time"
-
-	"github.com/hashicorp/vault/sdk/version"
 )
 
 func TestJsonTime(t *testing.T) {
@@ -32,51 +29,5 @@ func TestJsonTime(t *testing.T) {
 	}
 	if !now.Equal(time.Time(test2.Time)) {
 		t.Fatalf("expected: %s, got: %s", now, time.Time(test2.Time))
-	}
-}
-
-func TestUserAgent(t *testing.T) {
-	// VersionMetadata contains the version of Vault, typically "ent" or "prem" etc
-	// for enterprise versions, and "" for OSS version. Dev versions will contain
-	// "dev"
-	// GUID
-	// 15cd22ce-24af-43a4-aa83-4c1a36a4b177  Vault OSS
-	//
-	// b2c13ec1-60e8-4733-9a76-88dbb2ce2471  Vault Ent
-
-	testCases := map[string]struct {
-		meta     string
-		expected string
-	}{
-		"none": {
-			meta:     "",
-			expected: ossVaultGUID,
-		},
-		"dev": {
-			meta:     "dev",
-			expected: ossVaultGUID,
-		},
-		"ent": {
-			meta:     "ent",
-			expected: entVaultGUID,
-		},
-		"prem": {
-			meta:     "prem.hsm",
-			expected: entVaultGUID,
-		},
-		"unknown": {
-			meta:     "glhf",
-			expected: entVaultGUID,
-		},
-	}
-
-	for n, tc := range testCases {
-		t.Run(n, func(t *testing.T) {
-			version.VersionMetadata = tc.meta
-			userAgentStr := userAgent()
-			if !strings.Contains(userAgentStr, tc.expected) {
-				t.Fatalf("expected userAgent string to contain (%s), got: %s", tc.expected, userAgentStr)
-			}
-		})
 	}
 }
