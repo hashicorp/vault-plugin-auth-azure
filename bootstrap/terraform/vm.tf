cd179cdc859e7e7aa5dc124afe038562930399f9
Austin Gebauer <agebauer@hashicorp.com>
2022-11-04 16:35:46 -0700

    remove required_providers

--- bootstrap/terraform/vm.tf
+++ bootstrap/terraform/vm.tf
@@ -1,20 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.29.1"
-    }
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = ">= 2.30.0"
-    }
-    http = {
-      source  = "hashicorp/http"
-      version = ">= 3.2.0"
-    }
-  }
-}
-
 data "azurerm_client_config" "current" {}
 provider "azuread" {}
 provider "azurerm" {
