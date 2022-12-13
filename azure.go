c3d37bb4ed20ac6516fd05ee133fee95283c3713
vinay-gopalan <86625824+vinay-gopalan@users.noreply.github.com>
2022-12-12 17:03:36 -0800

    Remove `go-autorest` dependency from codebase (#72)

--- azure.go
+++ azure.go
@@ -8,14 +8,15 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
-	"sync"
-	"time"
+	"strings"
 
-	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2021-11-01/compute"
-	"github.com/Azure/azure-sdk-for-go/services/msi/mgmt/2018-11-30/msi"
-	"github.com/Azure/go-autorest/autorest"
-	"github.com/Azure/go-autorest/autorest/azure"
-	"github.com/Azure/go-autorest/autorest/azure/auth"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/arm"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/cloud"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
+	az "github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/compute/armcompute/v4"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/msi/armmsi"
 	"github.com/coreos/go-oidc"
 	"github.com/hashicorp/errwrap"
 	"github.com/hashicorp/go-cleanhttp"
@@ -23,18 +24,16 @@ import (
 	"golang.org/x/oauth2"
 )
 
-var authorizerLifetime = 30 * time.Minute
-
 type computeClient interface {
-	Get(ctx context.Context, resourceGroup, vmName string, instanceView compute.InstanceViewTypes) (compute.VirtualMachine, error)
+	Get(ctx context.Context, resourceGroupName string, vmName string, options *armcompute.VirtualMachinesClientGetOptions) (armcompute.VirtualMachinesClientGetResponse, error)
 }
 
 type vmssClient interface {
-	Get(ctx context.Context, resourceGroup, vmssName string, expandTypes compute.ExpandTypesForGetVMScaleSets) (compute.VirtualMachineScaleSet, error)
+	Get(ctx context.Context, resourceGroupName string, vmScaleSetName string, options *armcompute.VirtualMachineScaleSetsClientGetOptions) (armcompute.VirtualMachineScaleSetsClientGetResponse, error)
 }
 
 type msiClient interface {
-	Get(ctx context.Context, resourceGroup, resourceName string) (result msi.Identity, err error)
+	Get(ctx context.Context, resourceGroupName string, resourceName string, options *armmsi.UserAssignedIdentitiesClientGetOptions) (armmsi.UserAssignedIdentitiesClientGetResponse, error)
 }
 
 type tokenVerifier interface {
@@ -49,17 +48,37 @@ type provider interface {
 }
 
 type azureProvider struct {
-	oidcVerifier         *oidc.IDTokenVerifier
-	settings             *azureSettings
-	httpClient           *http.Client
-	authorizer           autorest.Authorizer
-	authorizerExpiration time.Time
-	lock                 sync.RWMutex
+	oidcVerifier *oidc.IDTokenVerifier
+	settings     *azureSettings
+	httpClient   *http.Client
 }
 
 type oidcDiscoveryInfo struct {
 	Issuer  string `json:"issuer"`
 	JWKSURL string `json:"jwks_uri"`
+}
+
+// transporter implements the azure exported.Transporter interface to send HTTP
+// requests. This allows us to set our custom http client and user agent.
+type transporter struct {
+	pluginEnv *logical.PluginEnvironment
+	sender    *http.Client
+}
+
+func (tp transporter) Do(req *http.Request) (*http.Response, error) {
+	req.Header.Set("User-Agent", userAgent(tp.pluginEnv))
+
+	client := tp.sender
+
+	// don't attempt redirects so we aren't acting as an unintended network proxy
+	client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
 }
 
 func (b *azureAuthBackend) newAzureProvider(ctx context.Context, config *azureConfig) (*azureProvider, error) {
@@ -73,7 +92,7 @@ func (b *azureAuthBackend) newAzureProvider(ctx context.Context, config *azureCo
 	// endpoint is the AD endpoint which does not match the issuer defined in the discovery payload. This
 	// makes a request to the discovery URL to determine the issuer and key set information to configure
 	// the OIDC verifier
-	discoveryURL := fmt.Sprintf("%s%s/.well-known/openid-configuration", settings.Environment.ActiveDirectoryEndpoint, settings.TenantID)
+	discoveryURL := fmt.Sprintf("%s%s/.well-known/openid-configuration", settings.CloudConfig.ActiveDirectoryAuthorityHost, settings.TenantID)
 	req, err := http.NewRequestWithContext(ctx, "GET", discoveryURL, nil)
 	if err != nil {
 		return nil, err
@@ -120,94 +139,97 @@ func (p *azureProvider) Verifier() tokenVerifier {
 }
 
 func (p *azureProvider) ComputeClient(subscriptionID string) (computeClient, error) {
-	authorizer, err := p.getAuthorizer()
+	cred, err := p.getTokenCredential()
 	if err != nil {
 		return nil, err
 	}
 
-	client := compute.NewVirtualMachinesClientWithBaseURI(p.settings.Environment.ResourceManagerEndpoint, subscriptionID)
-	client.Authorizer = authorizer
-	client.Sender = p.httpClient
-	client.AddToUserAgent(userAgent(p.settings.PluginEnv))
+	clientOptions := &arm.ClientOptions{
+		ClientOptions: policy.ClientOptions{
+			Cloud: p.settings.CloudConfig,
+			Transport: transporter{
+				pluginEnv: p.settings.PluginEnv,
+				sender:    p.httpClient,
+			},
+		},
+	}
+	client, err := armcompute.NewVirtualMachinesClient(subscriptionID, cred, clientOptions)
+	if err != nil {
+		return nil, err
+	}
+
 	return client, nil
 }
 
 func (p *azureProvider) VMSSClient(subscriptionID string) (vmssClient, error) {
-	authorizer, err := p.getAuthorizer()
+	cred, err := p.getTokenCredential()
 	if err != nil {
 		return nil, err
 	}
 
-	client := compute.NewVirtualMachineScaleSetsClientWithBaseURI(p.settings.Environment.ResourceManagerEndpoint, subscriptionID)
-	client.Authorizer = authorizer
-	client.Sender = p.httpClient
-	client.AddToUserAgent(userAgent(p.settings.PluginEnv))
+	clientOptions := &arm.ClientOptions{
+		ClientOptions: policy.ClientOptions{
+			Cloud: p.settings.CloudConfig,
+			Transport: transporter{
+				pluginEnv: p.settings.PluginEnv,
+				sender:    p.httpClient,
+			},
+		},
+	}
+	client, err := armcompute.NewVirtualMachineScaleSetsClient(subscriptionID, cred, clientOptions)
+	if err != nil {
+		return nil, err
+	}
+
 	return client, nil
 }
 
 func (p *azureProvider) MSIClient(subscriptionID string) (msiClient, error) {
-	authorizer, err := p.getAuthorizer()
+	cred, err := p.getTokenCredential()
 	if err != nil {
 		return nil, err
 	}
 
-	client := msi.NewUserAssignedIdentitiesClientWithBaseURI(p.settings.Environment.ResourceManagerEndpoint, subscriptionID)
-	client.Authorizer = authorizer
-	client.Sender = p.httpClient
-	client.AddToUserAgent(userAgent(p.settings.PluginEnv))
+	clientOptions := &arm.ClientOptions{
+		ClientOptions: policy.ClientOptions{
+			Cloud: p.settings.CloudConfig,
+			Transport: transporter{
+				pluginEnv: p.settings.PluginEnv,
+				sender:    p.httpClient,
+			},
+		},
+	}
+	client, err := armmsi.NewUserAssignedIdentitiesClient(subscriptionID, cred, clientOptions)
+	if err != nil {
+		return nil, err
+	}
+
 	return client, nil
 }
 
-func (p *azureProvider) getAuthorizer() (autorest.Authorizer, error) {
-	p.lock.RLock()
-	unlockFunc := p.lock.RUnlock
-	defer func() { unlockFunc() }()
-
-	if p.authorizer != nil && time.Now().Before(p.authorizerExpiration) {
-		return p.authorizer, nil
-	}
-
-	// Upgrade lock
-	p.lock.RUnlock()
-	p.lock.Lock()
-	unlockFunc = p.lock.Unlock
-
-	if p.authorizer != nil && time.Now().Before(p.authorizerExpiration) {
-		return p.authorizer, nil
-	}
-
-	// Create an OAuth2 client for retrieving VM data
-	var authorizer autorest.Authorizer
-	var err error
-	switch {
-	// Use environment/config first
-	case p.settings.ClientSecret != "":
-		config := auth.NewClientCredentialsConfig(p.settings.ClientID, p.settings.ClientSecret, p.settings.TenantID)
-		config.AADEndpoint = p.settings.Environment.ActiveDirectoryEndpoint
-		config.Resource = p.settings.Environment.ResourceManagerEndpoint
-		authorizer, err = config.Authorizer()
+func (p *azureProvider) getTokenCredential() (azcore.TokenCredential, error) {
+	if p.settings.ClientSecret != "" {
+		cred, err := az.NewClientSecretCredential(p.settings.TenantID, p.settings.ClientID, p.settings.ClientSecret, nil)
 		if err != nil {
 			return nil, err
 		}
-	// By default use MSI
-	default:
-		config := auth.NewMSIConfig()
-		config.Resource = p.settings.Environment.ResourceManagerEndpoint
-		authorizer, err = config.Authorizer()
+
+		return cred, nil
+	} else {
+		cred, err := az.NewManagedIdentityCredential(nil)
 		if err != nil {
 			return nil, err
 		}
+
+		return cred, nil
 	}
-	p.authorizer = authorizer
-	p.authorizerExpiration = time.Now().Add(authorizerLifetime)
-	return authorizer, nil
 }
 
 type azureSettings struct {
 	TenantID     string
 	ClientID     string
 	ClientSecret string
-	Environment  azure.Environment
+	CloudConfig  cloud.Configuration
 	Resource     string
 	PluginEnv    *logical.PluginEnvironment
 }
@@ -247,15 +269,15 @@ func (b *azureAuthBackend) getAzureSettings(ctx context.Context, config *azureCo
 	}
 	settings.ClientSecret = clientSecret
 
-	envName := os.Getenv("AZURE_ENVIRONMENT")
-	if envName == "" {
-		envName = config.Environment
+	configName := os.Getenv("AZURE_ENVIRONMENT")
+	if configName == "" {
+		configName = config.Environment
 	}
-	if envName == "" {
-		settings.Environment = azure.PublicCloud
+	if configName == "" {
+		settings.CloudConfig = cloud.AzurePublic
 	} else {
 		var err error
-		settings.Environment, err = azure.EnvironmentFromName(envName)
+		settings.CloudConfig, err = ConfigurationFromName(configName)
 		if err != nil {
 			return nil, err
 		}
@@ -268,4 +290,20 @@ func (b *azureAuthBackend) getAzureSettings(ctx context.Context, config *azureCo
 	settings.PluginEnv = pluginEnv
 
 	return settings, nil
+}
+
+func ConfigurationFromName(name string) (cloud.Configuration, error) {
+	configs := map[string]cloud.Configuration{
+		"AZURECHINACLOUD":        cloud.AzureChina,
+		"AZUREPUBLICCLOUD":       cloud.AzurePublic,
+		"AZUREUSGOVERNMENTCLOUD": cloud.AzureGovernment,
+	}
+
+	name = strings.ToUpper(name)
+	c, ok := configs[name]
+	if !ok {
+		return c, fmt.Errorf("err: no cloud configuration matching the name %q", name)
+	}
+
+	return c, nil
 }
--- go.mod
+++ go.mod
@@ -3,12 +3,7 @@ module github.com/hashicorp/vault-plugin-auth-azure
 go 1.19
 
 require (
-	github.com/Azure/azure-sdk-for-go v67.0.0+incompatible
-	github.com/Azure/go-autorest/autorest v0.11.28
-	github.com/Azure/go-autorest/autorest/azure/auth v0.5.11
-	github.com/Azure/go-autorest/autorest/to v0.4.0
 	github.com/coreos/go-oidc v2.2.1+incompatible
-	github.com/gofrs/uuid v4.3.1+incompatible
 	github.com/hashicorp/errwrap v1.1.0
 	github.com/hashicorp/go-cleanhttp v0.5.2
 	github.com/hashicorp/go-hclog v1.3.1
@@ -18,22 +13,21 @@ require (
 )
 
 require (
-	github.com/Azure/go-autorest v14.2.0+incompatible // indirect
-	github.com/Azure/go-autorest/autorest/adal v0.9.18 // indirect
-	github.com/Azure/go-autorest/autorest/azure/cli v0.4.5 // indirect
-	github.com/Azure/go-autorest/autorest/date v0.3.0 // indirect
-	github.com/Azure/go-autorest/autorest/validation v0.3.1 // indirect
-	github.com/Azure/go-autorest/logger v0.2.1 // indirect
-	github.com/Azure/go-autorest/tracing v0.6.0 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.0.0
+	github.com/Azure/azure-sdk-for-go/sdk/azidentity v1.2.0
+	github.com/Azure/azure-sdk-for-go/sdk/internal v1.0.0 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/compute/armcompute/v4 v4.0.0
+	github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/msi/armmsi v0.7.0
+	github.com/AzureAD/microsoft-authentication-library-for-go v0.7.0 // indirect
 	github.com/armon/go-metrics v0.3.10 // indirect
 	github.com/armon/go-radix v1.0.0 // indirect
 	github.com/cenkalti/backoff/v3 v3.2.2 // indirect
-	github.com/dimchansky/utfbom v1.1.1 // indirect
 	github.com/evanphx/json-patch/v5 v5.6.0 // indirect
 	github.com/fatih/color v1.13.0 // indirect
-	github.com/golang-jwt/jwt/v4 v4.2.0 // indirect
+	github.com/golang-jwt/jwt/v4 v4.4.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
+	github.com/google/uuid v1.1.2 // indirect
 	github.com/hashicorp/go-immutable-radix v1.3.1 // indirect
 	github.com/hashicorp/go-kms-wrapping/entropy/v2 v2.0.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
@@ -49,6 +43,7 @@ require (
 	github.com/hashicorp/golang-lru v0.5.4 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/hashicorp/yamux v0.0.0-20211028200310-0bc27b27de87 // indirect
+	github.com/kylelemons/godebug v1.1.0 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/mitchellh/copystructure v1.2.0 // indirect
@@ -58,6 +53,7 @@ require (
 	github.com/mitchellh/reflectwalk v1.0.2 // indirect
 	github.com/oklog/run v1.1.0 // indirect
 	github.com/pierrec/lz4 v2.6.1+incompatible // indirect
+	github.com/pkg/browser v0.0.0-20210115035449-ce105d075bb4 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pquerna/cachecontrol v0.1.0 // indirect
 	github.com/ryanuber/go-glob v1.0.0 // indirect
