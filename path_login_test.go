80e8ecbfecf19f1e1d4810570dd88dadafd4a299
Michael Blaum <michael.blaum@hashicorp.com>
2025-06-13 12:14:03 -0400

    update tests

--- path_login_test.go
+++ path_login_test.go
@@ -2162,7 +2162,7 @@ func Test_additionalClaims_verifyResourceGroup(t *testing.T) {
 			wantErr: assert.NoError,
 		},
 		{
-			name: "happy with uppercase resource groqup name",
+			name: "happy with uppercase resource group name",
 			fields: fields{
 				NotBefore: jsonTime(time.Now().Add(60 * time.Second)),
 				ObjectID:  principalID,
@@ -2171,6 +2171,23 @@ func Test_additionalClaims_verifyResourceGroup(t *testing.T) {
 			},
 			args: args{
 				resourceGroupName: strings.ToUpper(rgName),
+				resourceID: fmt.Sprintf(fmtResourceGroupID, subscriptionID, rgName,
+					"providers/Microsoft.Web",
+					"sites",
+					"my-azure-func"),
+			},
+			wantErr: assert.NoError,
+		},
+		{
+			name: "happy with mixed case resource group name",
+			fields: fields{
+				NotBefore: jsonTime(time.Now().Add(60 * time.Second)),
+				ObjectID:  principalID,
+				AppID:     appID,
+				GroupIDs:  []string{"test-group-1"},
+			},
+			args: args{
+				resourceGroupName: "rG",
 				resourceID: fmt.Sprintf(fmtResourceGroupID, subscriptionID, rgName,
 					"providers/Microsoft.Web",
 					"sites",
